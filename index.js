0cf3b271f53e08d3f3c4f72a899b7ebdb6ea3e16
yamatairiku-dev <yamatairiku.dev@gmail.com>
2023-06-03 19:18:40 +0900

    Where句改善

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ app.get('/todos', (req, res) => {
   } else if (completedQuery === 'false') {
     whereClause = { completed: false }
   } else {
-    whereClause = undefined
+    whereClause = {}
   }
   models.Todo.getTodoList(whereClause).then(todoListWithCount => {
     const todos = todoListWithCount.todoList
